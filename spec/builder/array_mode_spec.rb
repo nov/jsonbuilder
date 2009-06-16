787b8020c675feff0c6dbb55a3a7469ed3fc5e4d
Birkir A. Barkarson <birkirb@cerego.com>
2009-06-16 11:50:13 +0900

    Use new! for construction in the test for clarity, added a yet to be passed constructor free array_mode text.

--- spec/builder/array_mode_spec.rb
+++ spec/builder/array_mode_spec.rb
@@ -101,8 +101,7 @@ describe Builder::HashStructure, "#array_mode" do
       builder.items do
         builder.array_mode do
           2.times do |i|
-            _builder = Builder::HashStructure.new
-            builder << _builder.item(:id => i) do
+            builder << builder.new!.item(:id => i) do |_builder|
               _builder.text "hello world #{i}"
               _builder.sentence(:id => i) do
                 _builder.text "Hello world!!"
@@ -146,8 +145,7 @@ describe Builder::HashStructure, "#array_mode" do
       builder.items do
         builder.array_mode do
           2.times do |i|
-            _builder = Builder::HashStructure.new
-            builder << _builder.root!(:item, :id => i) do
+            builder << builder.new!.root!(:item, :id => i) do |_builder|
               _builder.text "hello world #{i}"
               _builder.sentence(:id => i) do
                 _builder.text "Hello world!!"
@@ -174,6 +172,53 @@ describe Builder::HashStructure, "#array_mode" do
     }
   end
 
+# it "should allow root! without constructing a new instance" do
+#   builder = Builder::HashStructure.new
+#   # XML ::
+#   # <root>
+#   #   <items>
+#   #     <item id="0">
+#   #       <text>hello world 0</text>
+#   #       <sentence id="0"></sentence>
+#   #     </item>
+#   #     <item id="0">
+#   #       <text>hello world 1</text>
+#   #       <sentence id="1">Hello world!!</sentence>
+#   #     </item>
+#   #   </items>
+#   # </root>
+#   builder.root do
+#     builder.items do
+#       builder.array_mode do
+#         2.times do |i|
+#           builder.root!(:item, :id => i) do
+#             builder.text "hello world #{i}"
+#             builder.sentence(:id => i) do
+#               builder.text "Hello world!!"
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+#   builder.target!.should == {
+#     :items => [{
+#       :item =>{
+#         :text => "hello world 0",
+#         :sentence => {:text => "Hello world!!", :id => 0},
+#         :id => 0
+#       }
+#     },{
+#       :item => {
+#         :text => "hello world 1",
+#         :sentence => {:text => "Hello world!!", :id => 1},
+#         :id => 1
+#       }
+#     }]
+#   }
+# end
+
+
   it "should generate a new key if needed" do
     builder = Builder::HashStructure.new
     # XML ::
@@ -189,7 +234,7 @@ describe Builder::HashStructure, "#array_mode" do
     # </root>
     builder.root do
       builder.items(:site => "smart.fm") do
-        builder.array_mode do
+        builder.array_mode(:item) do
           2.times do |i|
             builder.item do
               builder.text "hello world #{i}"
@@ -200,7 +245,7 @@ describe Builder::HashStructure, "#array_mode" do
     end
     builder.target!.should == {
       :items => {
-        :entry => [{:text => "hello world 0"}, {:text=>"hello world 1"}],
+        :item => [{:text => "hello world 0"}, {:text=>"hello world 1"}],
         :site=>"smart.fm"
       }
     }
